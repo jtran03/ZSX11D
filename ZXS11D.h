bb6b25a18312ca931d76b198f98381d664a83a65
jtran03 <60172315+jtran03@users.noreply.github.com>
2024-08-01 17:35:02 +1000

    Update ZXS11D.h

--- ZXS11D.h
+++ ZXS11D.h
@@ -6,9 +6,12 @@
 #include <QuickPID.h>
 
 #define PWM_FREQUENCY 5000
-#define PWM_RESOLUTION 8
-#define MIN_SPEED 24
-#define MAX_SPEED 255
+#define PWM_RESOLUTION 10
+#define MAX_RPM 361
+#define MIN_RPM 30
+#define MIN_PWM 0
+#define MAX_PWM 1024 // MUST BE < 2^PWM_RESOLUTION
+#define RISING_PPR 45 
 #define FORWARD 1
 #define STOP 0
 #define REVERSE -1
@@ -17,7 +20,7 @@
 #define NORMAL_TIMER_NUMBER 0
 #define REVERSED_TIMER_NUMBER 1
 #define TIMER_PRESCALAR 80
-#define MAX_TIMER_PERIOD_MICROSECONDS 333333 // 60/(RPMmin * PPR)
+#define MAX_TIMER_PERIOD_MICROSECONDS 200000 // 60/(RPMmin * PPR)
 
 #define KP 0.6
 #define KI 2.034
@@ -26,15 +29,20 @@
 class ZSX11D {
 public:
   ZSX11D(uint8_t pinPWMOut, uint8_t pinBrake, uint8_t pinDirection, uint8_t pinHallSensor, uint8_t pwmChannel, bool isReverse);
-  void begin(int PPR, int minRPM); 
-  void setPWM(int dutyCycle);
-  void setSpeed(int dutyCycle);
+  void begin(); 
+  void setPWM();
+  void setRPM(float RPM); 
   void stop(); 
   void reset(); 
-  float getFrequency();
+  float getRPM();
+  int getPulseCount(); 
   void loopPID(); 
-  float Setpoint, Input, Output; 
+  float getRPMSetpoint(); 
+  float getCurrentDutyCycle(); 
+  int getCurrentDirection(); 
   QuickPID myPID; 
+  
+
 private:
   uint8_t _pinPWMOut;
   uint8_t _pinBrake;
@@ -48,12 +56,15 @@ private:
   int _direction;
   int _maxPeriod; 
 
+  float Setpoint, Input, Output; 
+
   static ZSX11D* instances[2];
   volatile uint64_t _pulse_start, _pulse_end, _TOVF_Counter;
   volatile unsigned long _pulse_period;
   hw_timer_t *_timer;
   bool _measurement_end;
   unsigned long _last_pulse_time; 
+  volatile int _PPRCount; 
   static void IRAM_ATTR onLeftEncoderRise();
   static void IRAM_ATTR onLeftTimerOverflow();
   static void IRAM_ATTR onRightEncoderRise();
